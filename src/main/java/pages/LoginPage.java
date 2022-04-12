ef30cfad48d240ccf6061f14d6696eba54fa49e6
Dragos Tanta <dragos.tanta@gmail.com>
2022-04-12 22:27:13 +0300

    curs 14

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -16,12 +16,25 @@ public class LoginPage {
 	public By password = By.id("password");
 	public By loginButton = By.cssSelector("input[class='submit_button']");
 	
+	public By logoutButton = By.xpath("//li[@class='menu_user_logout']");
+	public By closePopUpButton = By.xpath("//a[@class='popup_close']");
+	
 	//metode
 	public void loginInApp(String user , String pass) {
 		driver.findElement(username).sendKeys(user);
 		driver.findElement(password).sendKeys(pass);
 		driver.findElement(loginButton).click();
 		
+	}
+	
+	public void logoutFromApp() {
+		driver.findElement(logoutButton).click();
+	}
+	
+	public void closePopUp() {
+		driver.findElement(username).clear();
+		driver.findElement(password).clear();
+		driver.findElement(closePopUpButton).click();
 	}
 	
 	
--- src/main/java/tests/LoginTest.java
+++ src/main/java/tests/LoginTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import pages.LoginPage;
@@ -8,13 +9,14 @@ import utils.BaseTest;
 
 public class LoginTest extends BaseTest{
 	
+	@Parameters({"user", "pass"})
 	@Test
-	public void loginTest() {
+	public void loginTest(String username, String password) {
 		NavigationMenuPage navMenu = new NavigationMenuPage(driver);
 		navMenu.navigateTo(navMenu.loginLink);
 		
 		LoginPage loginPage =  new LoginPage(driver);
-		loginPage.loginInApp("TestUser", "12345@67890");
+		loginPage.loginInApp(username, password);
 		
 		
 	}
